14f30397051e3c2d30874de6c905fc44e02e5362
fdemarque <demarque.ads@gmail.com>
2023-12-10 03:48:08 -0300

    implementação de verificação de userrole no método de Update

--- Api/UpdateApi.ts
+++ Api/UpdateApi.ts
@@ -7,41 +7,41 @@ import { Crypto } from "../Lib/Crypto";
 
 export class UpdateApi extends ApiClass {
     async DoRequest(user: User, request: any): Promise<any> {
-        const { name, artist} = request;
-
-        if (!name && !artist) {
-            // Se algum dos campos obrigatórios estiver ausente, retorne um erro
-            throw "Campos obrigatorios não fornecidos"
-        }
-
-        const { colunaAtualizar, novoValor } = request;
-
-        try {
+        
+        // Verificando permissão do usuário
+        if (user.Userrole != 1){
+            throw "Usuario não possui permissao para atualizar um registro";
+        }else{
+            const { name, artist } = request;
+    
+            if (!name && !artist) {
+                // Se algum dos campos obrigatórios estiver ausente, retorne um erro
+                throw "Campos obrigatorios não fornecidos"
+            }
+    
+            const { colunaAtualizar, novoValor } = request;
+    
             // Verificando se a música existe antes de tentar atualizar
             const musicExists = await Knex.getConnection()('music')
                 .select("*")
                 .where("nome", "ilike", `%${name}%`)
                 .andWhere("artista", "ilike", `%${artist}%`);
-
+    
             if (musicExists.length === 0) {
                 throw "Música não encontrada.";
             }
-
+    
             // Atualizando registro da música
             const updatedRows = await Knex.getConnection()('music')
                 .where("nome", "ilike", `%${name}%`)
                 .andWhere("artista", "ilike", `%${artist}%`)
                 .update({ [colunaAtualizar]: novoValor });
-
+    
             if (updatedRows > 0) {
                 return { success: true, message: "Atualização bem-sucedida." };
             } else {
                 return { success: false, message: "Nenhuma linha foi atualizada." };
             }
-        } catch (error) {
-            // Lidando com erros
-            console.error("Erro ao atualizar música:", error);
-            throw error;
         }
     }
 
